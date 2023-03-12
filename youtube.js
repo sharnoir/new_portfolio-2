7bade05e4ee2cab960f98c4854292846cd75a6e7
sharnoir <116141711+sharnoir@users.noreply.github.com>
2023-03-12 20:44:43 +0900

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,269 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player1 = new YT.Player('player1', {
+        videoId: '0Phj2gV8eIw',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': '0Phj2gV8eIw' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+
+
+    });
+var player2;
+    player2 = new YT.Player('player2', {
+        videoId:'sBo3I1m___s',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'sBo3I1m___s' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+
+
+    });
+
+    var player3;
+    player3 = new YT.Player('player3', {
+        videoId:'Xy4lBh-oxvI',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'Xy4lBh-oxvI' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+
+
+    });
+
+    var player4;
+    player4 = new YT.Player('player4', {
+        videoId:'iQ2imC0oqi4',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'iQ2imC0oqi4' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+    
+    var player5;
+    player5 = new YT.Player('player5', {
+        videoId:'6umagl5gUBE',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': '6umagl5gUBE' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+
+    var player6;
+    player6= new YT.Player('player6', {
+        videoId:'AmAVvUxkMSw',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'AmAVvUxkMSw' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+    var player7;
+    player7 = new YT.Player('player7', {
+        videoId:'ZwNFVtL_C7E',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'ZwNFVtL_C7E' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+    
+    var player8;
+    player8 = new YT.Player('player8', {
+        videoId:'QPhcvgKwws0',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'QPhcvgKwws0' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+    var player9;
+    player9 = new YT.Player('player9', {
+        videoId:'6Bx4Vk_x-Lc',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': '6Bx4Vk_x-Lc' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+    var player10;
+    player10 = new YT.Player('player10', {
+        videoId:'bdnlULjCEwM',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'bdnlULjCEwM' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+    var player11;
+    player11 = new YT.Player('player11', {
+        videoId:'ruWMhQOhhzc',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'ruWMhQOhhzc' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+    var player12;
+    player12 = new YT.Player('player12', {
+        videoId:'A6EzHF_d3Jc',
+        playerVars: {
+            'autoplay': true,
+            'controls': false,
+            'loop': true,
+            'playlist': 'A6EzHF_d3Jc' //반복재생할때 반드시 다시 한번 id를 적어주어야 한다.
+        },
+
+        events: {
+            //영상이 준비되면 함수가 실행
+            'onReady': function (event) {
+                event.target.mute(); //음소거, target은 영상자체
+            },
+
+        }
+
+
+    });
+
+
+
+
+
+
+
+
+};
